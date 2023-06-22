20eb18c2b63c59334ba24908dbc8a9aeb1122493
IntelSDM <GitBackup@shibemaster.me>
2023-06-22 13:23:30 +0100

    Button Redesign

--- GUI/Graphics/Entities/Button.cpp
+++ GUI/Graphics/Entities/Button.cpp
@@ -6,7 +6,7 @@
 Button::Button(float x, float y, std::string text, std::function<void()> click)
 {
 	Button::Pos = { x,y };
-	Button::Size = { 70,60 };
+	Button::Size = { 70,20 };
 	Button::Name = text;
 	Button::Action = click;
 	
@@ -35,8 +35,8 @@ void Button::Draw()
 		Button::SetVisible(false);
 	if (!Button::IsVisible())
 		return;
-
-	FilledRectangle(ParentPos.x + Button::Pos.x, ParentPos.y + Button::Pos.y, Button::Size.x, Button::Size.y, Colour(20, 30, 160, 255));
-	Text(Button::Name, ParentPos.x + Button::Pos.x + (Button::Size.x / 2), ParentPos.y + Button::Pos.y + (Button::Size.y / 2),12,"Verdana" ,Colour(240, 240, 240, 255), Centre);
+	OutlineRectangle(ParentPos.x + Pos.x, ParentPos.y + Pos.y, Size.x + 1, Size.y + 1, 1, Colour(130, 130, 130, 255));
+	FilledRectangle(ParentPos.x + Button::Pos.x, ParentPos.y + Button::Pos.y, Button::Size.x, Button::Size.y, Colour(80, 80, 80, 255));
+	Text(Button::Name, ParentPos.x + Button::Pos.x + (Button::Size.x / 2), ParentPos.y + Button::Pos.y + (Button::Size.y / 2),12,"Verdana" ,Colour(240, 240, 240, 255), CentreCentre);
 
 }
