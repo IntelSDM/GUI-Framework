9f2eb189c58bfc6c9014d132b114967c3b56575c
IntelSDM <GitBackup@shibemaster.me>
2023-06-22 19:13:30 +0100

    Preview Formatting

--- GUI/Graphics/Start Up/GUI.cpp
+++ GUI/Graphics/Start Up/GUI.cpp
@@ -13,7 +13,7 @@ int SelectedTab;
 int TabCount;
 bool ToggleTest;
 bool ToggleTest1 = true;
-std::string TextBoxText;
+std::string TextBoxText = "txtbox";
 int intvalue = 3;
 float floatvalue = 8.5f;
 void CreateGUI()
